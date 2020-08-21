6c4454102241246cb04e17f8170e73312253726a
sammoth <sammoth.th@gmail.com>
2020-08-21 18:43:35 +0100

    basic stubs that compile

---
+++ libpulse.spec
@@ -0,0 +1,148 @@
+# Generated from pulse/libpulse.dll by winedump
+
+
+
+5 cdecl pa_bytes_to_usec( long ptr ) LIBPULSE_pa_bytes_to_usec
+
+
+27 cdecl pa_context_connect( ptr str long ptr ) LIBPULSE_pa_context_connect
+28 cdecl pa_context_disconnect( ptr ) LIBPULSE_pa_context_disconnect
+29 cdecl pa_context_drain( ptr long ptr ) LIBPULSE_pa_context_drain
+30 cdecl pa_context_errno( ptr ) LIBPULSE_pa_context_errno
+
+
+47 cdecl pa_context_get_server( ptr ) LIBPULSE_pa_context_get_server
+48 cdecl pa_context_get_server_info( ptr long ptr ) LIBPULSE_pa_context_get_server_info
+49 cdecl pa_context_get_server_protocol_version( ptr ) LIBPULSE_pa_context_get_server_protocol_version
+
+60 cdecl pa_context_get_state( ptr ) LIBPULSE_pa_context_get_state
+
+62 cdecl pa_context_is_local( ptr ) LIBPULSE_pa_context_is_local
+63 cdecl pa_context_is_pending( ptr ) LIBPULSE_pa_context_is_pending
+
+72 cdecl pa_context_new( ptr str ) LIBPULSE_pa_context_new
+73 cdecl pa_context_new_with_proplist( ptr str ptr ) LIBPULSE_pa_context_new_with_proplist
+
+76 cdecl pa_context_proplist_remove( ptr str long ptr ) LIBPULSE_pa_context_proplist_remove
+77 cdecl pa_context_proplist_update( ptr long ptr long ptr ) LIBPULSE_pa_context_proplist_update
+78 cdecl pa_context_ref( ptr ) LIBPULSE_pa_context_ref
+
+88 cdecl pa_context_set_event_callback( ptr long ptr ) LIBPULSE_pa_context_set_event_callback
+89 cdecl pa_context_set_name( ptr str long ptr ) LIBPULSE_pa_context_set_name
+
+106 cdecl pa_context_set_state_callback( ptr long ptr ) LIBPULSE_pa_context_set_state_callback
+107 cdecl pa_context_set_subscribe_callback( ptr long ptr ) LIBPULSE_pa_context_set_subscribe_callback
+108 cdecl pa_context_stat( ptr long ptr ) LIBPULSE_pa_context_stat
+109 cdecl pa_context_subscribe( ptr long long ptr ) LIBPULSE_pa_context_subscribe
+
+115 cdecl pa_context_unref( ptr ) LIBPULSE_pa_context_unref
+
+
+
+195 cdecl pa_mainloop_dispatch( ptr ) LIBPULSE_pa_mainloop_dispatch
+196 cdecl pa_mainloop_free( ptr ) LIBPULSE_pa_mainloop_free
+197 cdecl pa_mainloop_get_api( ptr ) LIBPULSE_pa_mainloop_get_api
+198 cdecl pa_mainloop_get_retval( ptr ) LIBPULSE_pa_mainloop_get_retval
+199 cdecl pa_mainloop_iterate( ptr long ptr ) LIBPULSE_pa_mainloop_iterate
+200 cdecl pa_mainloop_new() LIBPULSE_pa_mainloop_new
+201 cdecl pa_mainloop_poll( ptr ) LIBPULSE_pa_mainloop_poll
+202 cdecl pa_mainloop_prepare( ptr long ) LIBPULSE_pa_mainloop_prepare
+203 cdecl pa_mainloop_quit( ptr long ) LIBPULSE_pa_mainloop_quit
+204 cdecl pa_mainloop_run( ptr ptr ) LIBPULSE_pa_mainloop_run
+205 cdecl pa_mainloop_set_poll_func( ptr long ptr ) LIBPULSE_pa_mainloop_set_poll_func
+206 cdecl pa_mainloop_wakeup( ptr ) LIBPULSE_pa_mainloop_wakeup
+207 cdecl pa_msleep( long ) LIBPULSE_pa_msleep
+208 cdecl pa_operation_cancel( ptr ) LIBPULSE_pa_operation_cancel
+209 cdecl pa_operation_get_state( ptr ) LIBPULSE_pa_operation_get_state
+210 cdecl pa_operation_ref( ptr ) LIBPULSE_pa_operation_ref
+211 cdecl pa_operation_unref( ptr ) LIBPULSE_pa_operation_unref
+
+214 cdecl pa_proplist_clear( ptr ) LIBPULSE_pa_proplist_clear
+215 cdecl pa_proplist_contains( ptr str ) LIBPULSE_pa_proplist_contains
+216 cdecl pa_proplist_copy( ptr ) LIBPULSE_pa_proplist_copy
+217 cdecl pa_proplist_equal( ptr ptr ) LIBPULSE_pa_proplist_equal
+218 cdecl pa_proplist_free( ptr ) LIBPULSE_pa_proplist_free
+219 cdecl pa_proplist_from_string( str ) LIBPULSE_pa_proplist_from_string
+220 cdecl pa_proplist_get( ptr str ptr ptr ) LIBPULSE_pa_proplist_get
+221 cdecl pa_proplist_gets( ptr str ) LIBPULSE_pa_proplist_gets
+222 cdecl pa_proplist_isempty( ptr ) LIBPULSE_pa_proplist_isempty
+223 cdecl pa_proplist_iterate( ptr ptr ) LIBPULSE_pa_proplist_iterate
+224 cdecl pa_proplist_new() LIBPULSE_pa_proplist_new
+225 cdecl pa_proplist_set( ptr str ptr long ) LIBPULSE_pa_proplist_set
+227 cdecl pa_proplist_setp( ptr str ) LIBPULSE_pa_proplist_setp
+228 cdecl pa_proplist_sets( ptr str str ) LIBPULSE_pa_proplist_sets
+229 cdecl pa_proplist_size( ptr ) LIBPULSE_pa_proplist_size
+230 cdecl pa_proplist_to_string( ptr ) LIBPULSE_pa_proplist_to_string
+231 cdecl pa_proplist_to_string_sep( ptr str ) LIBPULSE_pa_proplist_to_string_sep
+232 cdecl pa_proplist_unset( ptr str ) LIBPULSE_pa_proplist_unset
+233 cdecl pa_proplist_unset_many( ptr str ) LIBPULSE_pa_proplist_unset_many
+234 cdecl pa_proplist_update( ptr long ptr ) LIBPULSE_pa_proplist_update
+
+250 cdecl pa_stream_begin_write( ptr ptr ptr ) LIBPULSE_pa_stream_begin_write
+251 cdecl pa_stream_cancel_write( ptr ) LIBPULSE_pa_stream_cancel_write
+252 cdecl pa_stream_connect_playback( ptr str ptr long ptr ptr ) LIBPULSE_pa_stream_connect_playback
+255 cdecl pa_stream_cork( ptr long long ptr ) LIBPULSE_pa_stream_cork
+256 cdecl pa_stream_disconnect( ptr ) LIBPULSE_pa_stream_disconnect
+257 cdecl pa_stream_drain( ptr long ptr ) LIBPULSE_pa_stream_drain
+258 cdecl pa_stream_drop( ptr ) LIBPULSE_pa_stream_drop
+260 cdecl pa_stream_flush( ptr long ptr ) LIBPULSE_pa_stream_flush
+261 cdecl pa_stream_get_buffer_attr( ptr ) LIBPULSE_pa_stream_get_buffer_attr
+262 cdecl pa_stream_get_channel_map( ptr ) LIBPULSE_pa_stream_get_channel_map
+263 cdecl pa_stream_get_context( ptr ) LIBPULSE_pa_stream_get_context
+264 cdecl pa_stream_get_device_index( ptr ) LIBPULSE_pa_stream_get_device_index
+265 cdecl pa_stream_get_device_name( ptr ) LIBPULSE_pa_stream_get_device_name
+267 cdecl pa_stream_get_index( ptr ) LIBPULSE_pa_stream_get_index
+268 cdecl pa_stream_get_latency( ptr ptr ptr ) LIBPULSE_pa_stream_get_latency
+269 cdecl pa_stream_get_monitor_stream( ptr ) LIBPULSE_pa_stream_get_monitor_stream
+270 cdecl pa_stream_get_sample_spec( ptr ) LIBPULSE_pa_stream_get_sample_spec
+271 cdecl pa_stream_get_state( ptr ) LIBPULSE_pa_stream_get_state
+272 cdecl pa_stream_get_time( ptr ptr ) LIBPULSE_pa_stream_get_time
+273 cdecl pa_stream_get_timing_info( ptr ) LIBPULSE_pa_stream_get_timing_info
+274 cdecl pa_stream_get_underflow_index( ptr ) LIBPULSE_pa_stream_get_underflow_index
+275 cdecl pa_stream_is_corked( ptr ) LIBPULSE_pa_stream_is_corked
+276 cdecl pa_stream_is_suspended( ptr ) LIBPULSE_pa_stream_is_suspended
+277 cdecl pa_stream_new( ptr str ptr ptr ) LIBPULSE_pa_stream_new
+278 cdecl pa_stream_new_extended( ptr str ptr long ptr ) LIBPULSE_pa_stream_new_extended
+279 cdecl pa_stream_new_with_proplist( ptr str ptr ptr ptr ) LIBPULSE_pa_stream_new_with_proplist
+280 cdecl pa_stream_peek( ptr ptr ptr ) LIBPULSE_pa_stream_peek
+281 cdecl pa_stream_prebuf( ptr long ptr ) LIBPULSE_pa_stream_prebuf
+282 cdecl pa_stream_proplist_remove( ptr str long ptr ) LIBPULSE_pa_stream_proplist_remove
+283 cdecl pa_stream_proplist_update( ptr long ptr long ptr ) LIBPULSE_pa_stream_proplist_update
+284 cdecl pa_stream_readable_size( ptr ) LIBPULSE_pa_stream_readable_size
+285 cdecl pa_stream_ref( ptr ) LIBPULSE_pa_stream_ref
+286 cdecl pa_stream_set_buffer_attr( ptr ptr long ptr ) LIBPULSE_pa_stream_set_buffer_attr
+287 cdecl pa_stream_set_buffer_attr_callback( ptr long ptr ) LIBPULSE_pa_stream_set_buffer_attr_callback
+288 cdecl pa_stream_set_event_callback( ptr long ptr ) LIBPULSE_pa_stream_set_event_callback
+289 cdecl pa_stream_set_latency_update_callback( ptr long ptr ) LIBPULSE_pa_stream_set_latency_update_callback
+290 cdecl pa_stream_set_monitor_stream( ptr long ) LIBPULSE_pa_stream_set_monitor_stream
+291 cdecl pa_stream_set_moved_callback( ptr long ptr ) LIBPULSE_pa_stream_set_moved_callback
+292 cdecl pa_stream_set_name( ptr str long ptr ) LIBPULSE_pa_stream_set_name
+293 cdecl pa_stream_set_overflow_callback( ptr long ptr ) LIBPULSE_pa_stream_set_overflow_callback
+294 cdecl pa_stream_set_read_callback( ptr long ptr ) LIBPULSE_pa_stream_set_read_callback
+295 cdecl pa_stream_set_started_callback( ptr long ptr ) LIBPULSE_pa_stream_set_started_callback
+296 cdecl pa_stream_set_state_callback( ptr long ptr ) LIBPULSE_pa_stream_set_state_callback
+297 cdecl pa_stream_set_suspended_callback( ptr long ptr ) LIBPULSE_pa_stream_set_suspended_callback
+298 cdecl pa_stream_set_underflow_callback( ptr long ptr ) LIBPULSE_pa_stream_set_underflow_callback
+299 cdecl pa_stream_set_write_callback( ptr long ptr ) LIBPULSE_pa_stream_set_write_callback
+300 cdecl pa_stream_trigger( ptr long ptr ) LIBPULSE_pa_stream_trigger
+301 cdecl pa_stream_unref( ptr ) LIBPULSE_pa_stream_unref
+302 cdecl pa_stream_update_sample_rate( ptr long long ptr ) LIBPULSE_pa_stream_update_sample_rate
+303 cdecl pa_stream_update_timing_info( ptr long ptr ) LIBPULSE_pa_stream_update_timing_info
+304 cdecl pa_stream_writable_size( ptr ) LIBPULSE_pa_stream_writable_size
+305 cdecl pa_stream_write( ptr ptr long long long long ) LIBPULSE_pa_stream_write
+306 cdecl pa_strerror( long ) LIBPULSE_pa_strerror
+
+319 cdecl pa_threaded_mainloop_accept( ptr ) LIBPULSE_pa_threaded_mainloop_accept
+320 cdecl pa_threaded_mainloop_free( ptr ) LIBPULSE_pa_threaded_mainloop_free
+321 cdecl pa_threaded_mainloop_get_api( ptr ) LIBPULSE_pa_threaded_mainloop_get_api
+322 cdecl pa_threaded_mainloop_get_retval( ptr ) LIBPULSE_pa_threaded_mainloop_get_retval
+323 cdecl pa_threaded_mainloop_in_thread( ptr ) LIBPULSE_pa_threaded_mainloop_in_thread
+324 cdecl pa_threaded_mainloop_lock( ptr ) LIBPULSE_pa_threaded_mainloop_lock
+325 cdecl pa_threaded_mainloop_new() LIBPULSE_pa_threaded_mainloop_new
+326 cdecl pa_threaded_mainloop_signal( ptr long ) LIBPULSE_pa_threaded_mainloop_signal
+327 cdecl pa_threaded_mainloop_start( ptr ) LIBPULSE_pa_threaded_mainloop_start
+328 cdecl pa_threaded_mainloop_stop( ptr ) LIBPULSE_pa_threaded_mainloop_stop
+329 cdecl pa_threaded_mainloop_unlock( ptr ) LIBPULSE_pa_threaded_mainloop_unlock
+330 cdecl pa_threaded_mainloop_wait( ptr ) LIBPULSE_pa_threaded_mainloop_wait
+
+338 cdecl pa_usec_to_bytes( long ptr ) LIBPULSE_pa_usec_to_bytes
---
+++ libpulse_dll.h
@@ -0,0 +1,154 @@
+/*
+ * libpulse.dll
+ *
+ * Generated from libpulse.dll by winedump.
+ *
+ * DO NOT SEND GENERATED DLLS FOR INCLUSION INTO WINE !
+ *
+ */
+#ifndef __WINE_LIBPULSE_DLL_H
+#define __WINE_LIBPULSE_DLL_H
+
+#include "windef.h"
+#include "wine/debug.h"
+#include "winbase.h"
+#include "winnt.h"
+#include "pulseaudio.h"
+
+
+pa_usec_t __cdecl LIBPULSE_pa_bytes_to_usec(uint64_t length, pa_sample_spec * spec);
+
+int __cdecl LIBPULSE_pa_context_connect(pa_context * c, char * server, pa_context_flags_t flags, pa_spawn_api * api);
+void __cdecl LIBPULSE_pa_context_disconnect(pa_context * c);
+pa_operation * __cdecl LIBPULSE_pa_context_drain(pa_context * c, pa_context_notify_cb_t cb, void * userdata);
+int __cdecl LIBPULSE_pa_context_errno(pa_context * c);
+
+char * __cdecl LIBPULSE_pa_context_get_server(pa_context * c);
+pa_operation * __cdecl LIBPULSE_pa_context_get_server_info(pa_context * c, pa_server_info_cb_t cb, void * userdata);
+uint32_t __cdecl LIBPULSE_pa_context_get_server_protocol_version(pa_context * c);
+pa_context_state_t __cdecl LIBPULSE_pa_context_get_state(pa_context * c);
+int __cdecl LIBPULSE_pa_context_is_local(pa_context * c);
+int __cdecl LIBPULSE_pa_context_is_pending(pa_context * c);
+pa_context * __cdecl LIBPULSE_pa_context_new(pa_mainloop_api * mainloop, char * name);
+pa_context * __cdecl LIBPULSE_pa_context_new_with_proplist(pa_mainloop_api * mainloop, char * name, pa_proplist * p);
+pa_operation * __cdecl LIBPULSE_pa_context_proplist_remove(pa_context * c, char * keys, pa_context_success_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_context_proplist_update(pa_context * c, pa_update_mode_t mode, pa_proplist * p, pa_context_success_cb_t cb, void * userdata);
+pa_context * __cdecl LIBPULSE_pa_context_ref(pa_context * c);
+
+void __cdecl LIBPULSE_pa_context_set_event_callback(pa_context * p, pa_context_event_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_context_set_name(pa_context * c, char * name, pa_context_success_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_context_set_state_callback(pa_context * c, pa_context_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_context_set_subscribe_callback(pa_context * c, pa_context_subscribe_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_context_stat(pa_context * c, pa_stat_info_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_context_subscribe(pa_context * c, pa_subscription_mask_t m, pa_context_success_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_context_unref(pa_context * c);
+
+int __cdecl LIBPULSE_pa_mainloop_dispatch(pa_mainloop * m);
+void __cdecl LIBPULSE_pa_mainloop_free(pa_mainloop * m);
+pa_mainloop_api * __cdecl LIBPULSE_pa_mainloop_get_api(pa_mainloop * m);
+int __cdecl LIBPULSE_pa_mainloop_get_retval(pa_mainloop * m);
+int __cdecl LIBPULSE_pa_mainloop_iterate(pa_mainloop * m, int block, int * retval);
+pa_mainloop * __cdecl LIBPULSE_pa_mainloop_new(void);
+int __cdecl LIBPULSE_pa_mainloop_poll(pa_mainloop * m);
+int __cdecl LIBPULSE_pa_mainloop_prepare(pa_mainloop * m, int timeout);
+void __cdecl LIBPULSE_pa_mainloop_quit(pa_mainloop * m, int retval);
+int __cdecl LIBPULSE_pa_mainloop_run(pa_mainloop * m, int * retval);
+void __cdecl LIBPULSE_pa_mainloop_set_poll_func(pa_mainloop * m, pa_poll_func poll_func, void * userdata);
+void __cdecl LIBPULSE_pa_mainloop_wakeup(pa_mainloop * m);
+int __cdecl LIBPULSE_pa_msleep(unsigned long t);
+void __cdecl LIBPULSE_pa_operation_cancel(pa_operation * o);
+pa_operation_state_t __cdecl LIBPULSE_pa_operation_get_state(pa_operation * o);
+pa_operation * __cdecl LIBPULSE_pa_operation_ref(pa_operation * o);
+void __cdecl LIBPULSE_pa_operation_unref(pa_operation * o);
+
+void __cdecl LIBPULSE_pa_proplist_clear(pa_proplist * p);
+int __cdecl LIBPULSE_pa_proplist_contains(pa_proplist * p, char * key);
+pa_proplist * __cdecl LIBPULSE_pa_proplist_copy(pa_proplist * p);
+int __cdecl LIBPULSE_pa_proplist_equal(pa_proplist * a, pa_proplist * b);
+void __cdecl LIBPULSE_pa_proplist_free(pa_proplist * p);
+pa_proplist * __cdecl LIBPULSE_pa_proplist_from_string(char * s);
+int __cdecl LIBPULSE_pa_proplist_get(pa_proplist * p, char * key, void ** data, size_t * nbytes);
+char * __cdecl LIBPULSE_pa_proplist_gets(pa_proplist * p, char * key);
+int __cdecl LIBPULSE_pa_proplist_isempty(pa_proplist * p);
+char * __cdecl LIBPULSE_pa_proplist_iterate(pa_proplist * p, void ** state);
+pa_proplist * __cdecl LIBPULSE_pa_proplist_new(void);
+int __cdecl LIBPULSE_pa_proplist_set(pa_proplist * p, char * key, void * data, size_t nbytes);
+int __cdecl LIBPULSE_pa_proplist_setp(pa_proplist * p, char * pair);
+int __cdecl LIBPULSE_pa_proplist_sets(pa_proplist * p, char * key, char * value);
+unsigned int __cdecl LIBPULSE_pa_proplist_size(pa_proplist * p);
+char * __cdecl LIBPULSE_pa_proplist_to_string(pa_proplist * p);
+char * __cdecl LIBPULSE_pa_proplist_to_string_sep(pa_proplist * p, char * sep);
+int __cdecl LIBPULSE_pa_proplist_unset(pa_proplist * p, char * key);
+int __cdecl LIBPULSE_pa_proplist_unset_many(pa_proplist * p, char * keys);
+void __cdecl LIBPULSE_pa_proplist_update(pa_proplist * p, pa_update_mode_t mode, pa_proplist * other);
+
+
+int __cdecl LIBPULSE_pa_stream_begin_write(pa_stream * s, void ** data, size_t * nbytes);
+int __cdecl LIBPULSE_pa_stream_cancel_write(pa_stream * s);
+int __cdecl LIBPULSE_pa_stream_connect_playback(pa_stream * s, char * dev, pa_buffer_attr * attr, pa_stream_flags_t flags, pa_cvolume * volume, pa_stream * sync_stream);
+pa_operation * __cdecl LIBPULSE_pa_stream_cork(pa_stream * s, int b, pa_stream_success_cb_t cb, void * userdata);
+int __cdecl LIBPULSE_pa_stream_disconnect(pa_stream * s);
+pa_operation * __cdecl LIBPULSE_pa_stream_drain(pa_stream * s, pa_stream_success_cb_t cb, void * userdata);
+int __cdecl LIBPULSE_pa_stream_drop(pa_stream * s);
+pa_operation * __cdecl LIBPULSE_pa_stream_flush(pa_stream * s, pa_stream_success_cb_t cb, void * userdata);
+pa_buffer_attr * __cdecl LIBPULSE_pa_stream_get_buffer_attr(pa_stream * s);
+pa_channel_map * __cdecl LIBPULSE_pa_stream_get_channel_map(pa_stream * s);
+pa_context * __cdecl LIBPULSE_pa_stream_get_context(pa_stream * s);
+uint32_t __cdecl LIBPULSE_pa_stream_get_index(pa_stream * s);
+int __cdecl LIBPULSE_pa_stream_get_latency(pa_stream * s, pa_usec_t * r_usec, int * negative);
+uint32_t __cdecl LIBPULSE_pa_stream_get_monitor_stream(pa_stream * s);
+pa_sample_spec * __cdecl LIBPULSE_pa_stream_get_sample_spec(pa_stream * s);
+pa_stream_state_t __cdecl LIBPULSE_pa_stream_get_state(pa_stream * s);
+int __cdecl LIBPULSE_pa_stream_get_time(pa_stream * s, pa_usec_t * r_usec);
+pa_timing_info * __cdecl LIBPULSE_pa_stream_get_timing_info(pa_stream * s);
+int64_t __cdecl LIBPULSE_pa_stream_get_underflow_index(pa_stream * p);
+int __cdecl LIBPULSE_pa_stream_is_corked(pa_stream * s);
+int __cdecl LIBPULSE_pa_stream_is_suspended(pa_stream * s);
+pa_stream * __cdecl LIBPULSE_pa_stream_new(pa_context * c, char * name, pa_sample_spec * ss, pa_channel_map * map);
+pa_stream * __cdecl LIBPULSE_pa_stream_new_extended(pa_context * c, char * name, pa_format_info * arg2, unsigned int n_formats, pa_proplist * p);
+pa_stream * __cdecl LIBPULSE_pa_stream_new_with_proplist(pa_context * c, char * name, pa_sample_spec * ss, pa_channel_map * map, pa_proplist * p);
+int __cdecl LIBPULSE_pa_stream_peek(pa_stream * s, void ** data, size_t * length);
+pa_operation * __cdecl LIBPULSE_pa_stream_prebuf(pa_stream * s, pa_stream_success_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_stream_proplist_remove(pa_stream * s, char * keys, pa_stream_success_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_stream_proplist_update(pa_stream * s, pa_update_mode_t mode, pa_proplist * p, pa_stream_success_cb_t cb, void * userdata);
+size_t __cdecl LIBPULSE_pa_stream_readable_size(pa_stream * s);
+pa_stream * __cdecl LIBPULSE_pa_stream_ref(pa_stream * s);
+pa_operation * __cdecl LIBPULSE_pa_stream_set_buffer_attr(pa_stream * s, pa_buffer_attr * attr, pa_stream_success_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_buffer_attr_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_event_callback(pa_stream * s, pa_stream_event_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_latency_update_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+
+void __cdecl LIBPULSE_pa_stream_set_moved_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_stream_set_name(pa_stream * s, char * name, pa_stream_success_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_overflow_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_read_callback(pa_stream * s, pa_stream_request_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_started_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_state_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_suspended_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_underflow_callback(pa_stream * s, pa_stream_notify_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_set_write_callback(pa_stream * s, pa_stream_request_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_stream_trigger(pa_stream * s, pa_stream_success_cb_t cb, void * userdata);
+void __cdecl LIBPULSE_pa_stream_unref(pa_stream * s);
+pa_operation * __cdecl LIBPULSE_pa_stream_update_sample_rate(pa_stream * s, uint32_t rate, pa_stream_success_cb_t cb, void * userdata);
+pa_operation * __cdecl LIBPULSE_pa_stream_update_timing_info(pa_stream * s, pa_stream_success_cb_t cb, void * userdata);
+size_t __cdecl LIBPULSE_pa_stream_writable_size(pa_stream * s);
+int __cdecl LIBPULSE_pa_stream_write(pa_stream * s, void * data, size_t length, pa_free_cb_t free_cb, int64_t offset, pa_seek_mode_t seek);
+char * __cdecl LIBPULSE_pa_strerror(int error);
+
+void __cdecl LIBPULSE_pa_threaded_mainloop_accept(pa_threaded_mainloop * m);
+void __cdecl LIBPULSE_pa_threaded_mainloop_free(pa_threaded_mainloop * m);
+pa_mainloop_api * __cdecl LIBPULSE_pa_threaded_mainloop_get_api(pa_threaded_mainloop * m);
+int __cdecl LIBPULSE_pa_threaded_mainloop_get_retval(pa_threaded_mainloop * m);
+int __cdecl LIBPULSE_pa_threaded_mainloop_in_thread(pa_threaded_mainloop * m);
+void __cdecl LIBPULSE_pa_threaded_mainloop_lock(pa_threaded_mainloop * m);
+pa_threaded_mainloop * __cdecl LIBPULSE_pa_threaded_mainloop_new(void);
+void __cdecl LIBPULSE_pa_threaded_mainloop_signal(pa_threaded_mainloop * m, int wait_for_accept);
+int __cdecl LIBPULSE_pa_threaded_mainloop_start(pa_threaded_mainloop * m);
+void __cdecl LIBPULSE_pa_threaded_mainloop_stop(pa_threaded_mainloop * m);
+void __cdecl LIBPULSE_pa_threaded_mainloop_unlock(pa_threaded_mainloop * m);
+void __cdecl LIBPULSE_pa_threaded_mainloop_wait(pa_threaded_mainloop * m);
+
+size_t __cdecl LIBPULSE_pa_usec_to_bytes(pa_usec_t t, pa_sample_spec * spec);
+
+
+#endif	/* __WINE_LIBPULSE_DLL_H */
